bea4921af0d4e5d409bc621c9dbb957369e92729
Artyom Sorokoumov <artyom.sorokoumov@gmail.com>
2018-04-24 13:14:30 +0300

    refactoring

--- main.cpp
+++ main.cpp
@@ -1,103 +1,66 @@
-#include <iostream>
 #include <vector>
-
+#include <string>
 #include <dlib/clustering.h>
-#include <dlib/rand.h>
 #include <dlib/image_io.h>
-#include <cmath>
+#include <dlib/image_transforms.h>
 
-#include "points.h"
-
-using namespace std;
-using namespace dlib;
-
-using sample_type = matrix<double,2,1>;
-using kernel_type = radial_basis_kernel<sample_type>;
-
-
-class color_storage
-{
-  public:
-    color_storage(int clusters_count)
-    {
-      int colors_count = clusters_count / 3;
-      int base_r = 0x00;
-      int base_g = 0x00;
-      int base_b = 0x00;
-
-      int divisor= std::cbrt(clusters_count) + 1.0; /* cube root */
-      std::uint8_t step_r=(255 - base_r) / (int)divisor;
-      std::uint8_t step_g=(255 - base_g) / (int)divisor;
-      std::uint8_t step_b=(255 - base_b) / (int)divisor;
-      for(std::uint8_t r=255; r >= base_r + step_r; r-=step_r)
-      {
-        for(std::uint8_t g=255; g >= base_g + step_g; g-=step_g)
-        {
-          for(std::uint8_t b=255; b >= base_b + step_b; b-=step_b)
-          {
-            m_colors.push_back({r,g,b});
-          }
-        }
-      }
-    }
-
-    rgb_pixel get_color(int cluster_number)
-    {
-      return m_colors.at(cluster_number);
-    }
-  private:
-    std::vector<rgb_pixel> m_colors;
-};
-
+using sample_type = dlib::matrix<double, 2, 1>;
+using kernel_type = dlib::radial_basis_kernel<sample_type>;
 
 int main(int argc, char **argv)
 {
-  //if(argc == 2)
+  if(argc == 2)
   {
-    //int clusters_num = std::stoi(argv[1]);
-    int clusters_num = 5;
+    unsigned long clusters_num = std::stoul(argv[1]);
 
     std::vector<sample_type> points;
     sample_type m;
-    for(int i = 0; i < data_set.size(); i++)
+    std::string point_coord;
+    std::string coord;
+    while(std::getline(std::cin, point_coord))
     {
-      m(0) = data_set.at(i).first;
-      m(1) = data_set.at(i).second;
+      std::stringstream input(point_coord);
+      for(int i = 0; i < 2; i++)
+      {
+        std::getline(input, coord, ';');
+        m(i) = std::stod(coord);
+      }
       points.push_back(m);
     }
 
-    kcentroid<kernel_type> kc(kernel_type(0.1), 0.01, 8);
-    kkmeans<kernel_type> test(kc);
+    dlib::kcentroid<kernel_type> kc(kernel_type(0.0001), 0.001, clusters_num);
+    dlib::kkmeans<kernel_type> test(kc);
     test.set_number_of_centers(clusters_num);
     std::vector<sample_type> initial_centers;
     pick_initial_centers(clusters_num, initial_centers, points, test.get_kernel());
     test.train(points, initial_centers);
 
     std::map<int, std::vector<sample_type>> clustered_map;
-    for (unsigned long i = 0; i < points.size(); i++)
+    for (const auto &point : points)
     {
-      int c = test(points[i]);
-      clustered_map[c].push_back(points[i]);
+      unsigned long c = test(point);
+      clustered_map[c].push_back(point);
     }
-    cout << endl;
 
-    color_storage cs(clusters_num);
-    array2d<rgb_pixel> img(200, 200);
+    dlib::array2d<dlib::rgb_pixel> img(200, 200);
+    for(int i = 0; i < 200; i++)
+      for(int j = 0; j < 200; j++)
+        img[i][j] = dlib::rgb_pixel(255, 255, 255);
+
     int i = 0;
     for(auto c : clustered_map)
     {
       int cluster = c.first;
       for(auto point : c.second)
       {
-        int x = point(0);
-        int y = point(1);
-        img[x][y] = cs.get_color(cluster);
+        double x = point(0) + 100;
+        double y = point(1) + 100;
+        img[x][y] = dlib::colormap_jet(cluster, 0, clusters_num);
         i++;
       }
     }
     dlib::save_png(img, "./kmeans.png");
   }
-
 
   return 0;
 }
